4d1da984c6059b2b5ff71bad91dd594d8c3cc9e5
im4-pushpan <push26111997@gmail.com>
2024-06-13 08:09:09 +0530

    Core logic

--- app/src/main/java/com/example/notificationlistener/MainActivity.java
+++ app/src/main/java/com/example/notificationlistener/MainActivity.java
@@ -4,12 +4,23 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private Button buttonRequestAccess;
+
+    private Boolean isLoggedIn = false;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -17,12 +28,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button_request_access).setOnClickListener(new View.OnClickListener() {
+        editTextUsername= findViewById(R.id.editText_username);
+        editTextPassword= findViewById(R.id.editText_password);
+        buttonLogin= findViewById(R.id.button_login);
+        buttonRequestAccess= findViewById(R.id.button_request_access);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent[] intent = Arrays.asList(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)).toArray(new Intent[0]);
+                handleLogin();
+            }
+        });
+        buttonRequestAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent[] intent = Collections.singletonList(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)).toArray(new Intent[0]);
                 startActivities(intent);
             }
         });
+        updateUIBasedOnLoginState();
+    }
+
+    private void updateUIBasedOnLoginState() {
+
+        if(isLoggedIn){
+            editTextUsername.setVisibility(View.GONE);
+            editTextPassword.setVisibility(View.GONE);
+            buttonLogin.setVisibility(View.GONE);
+            buttonRequestAccess.setVisibility(View.VISIBLE);
+        }else{
+            editTextUsername.setVisibility(View.VISIBLE);
+            editTextPassword.setVisibility(View.VISIBLE);
+            buttonLogin.setVisibility(View.VISIBLE);
+            buttonRequestAccess.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void handleLogin() {
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if(username.equals("admin") && password.equals("password")){
+            isLoggedIn=true;
+            Toast.makeText(this,"login successful",Toast.LENGTH_SHORT).show();
+            updateUIBasedOnLoginState();
+        }
     }
 }
--- app/src/main/java/com/example/notificationlistener/MyNotificationListenerService.java
+++ app/src/main/java/com/example/notificationlistener/MyNotificationListenerService.java
@@ -4,6 +4,12 @@ import android.app.Notification;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
+import android.widget.Toast;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MyNotificationListenerService extends NotificationListenerService {
 
@@ -19,6 +25,24 @@ public class MyNotificationListenerService extends NotificationListenerService {
             Log.i(TAG,"Notification posted from: " + packageName);
             Log.i(TAG,"Title: "+ title);
             Log.i(TAG,"Text: "+ text);
+            if(text !=null) {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("http://ipAdresss:8080")   //cmd ipConfig
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                TransactionApi transactionApi = retrofit.create(TransactionApi.class);
+                transactionApi.save(text.toString()).enqueue(new Callback<Object>() {
+                    @Override
+                    public void onResponse(Call<Object> call, Response<Object> response) {
+                        Toast.makeText(MyNotificationListenerService.this, "successful", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Object> call, Throwable throwable) {
+                        Toast.makeText(MyNotificationListenerService.this, "failed successful", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         }
     }
 
